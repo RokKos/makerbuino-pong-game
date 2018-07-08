86aac9ec9184fb193cb34f5f0b54394fe816b971
Rok Kos <rocki5555@gmail.com>
2018-07-08 20:45:19 +0200

    Improved script so that now deletes all files except two

--- src/pong_game/rename.sh
+++ src/pong_game/rename.sh
@@ -2,6 +2,7 @@
 #Script that deletes unecesary binaries, renames and moves binaries that Arduino IDE created
 
 game_name=PONG2P.HEX
+game_description=PONG2P.INF
 game_binary=pong_game.ino.hex
 #This command compiles sketch into hex file
 #/home/rokkos/arduino-1.8.5/arduino-builder -compile -logger=machine -hardware /home/rokkos/arduino-1.8.5/hardware -tools /home/rokkos/arduino-1.8.5/tools-builder -tools /home/rokkos/arduino-1.8.5/hardware/tools/avr -built-in-libraries /home/rokkos/arduino-1.8.5/libraries -libraries /home/rokkos/Arduino/libraries -fqbn=arduino:avr:uno -ide-version=10805 -build-path /home/rokkos -warnings=none -build-cache /tmp/arduino_cache_966880 -prefs=build.warn_data_percentage=75 -prefs=runtime.tools.avr-gcc.path=/home/rokkos/arduino-1.8.5/hardware/tools/avr -prefs=runtime.tools.arduinoOTA.path=/home/rokkos/arduino-1.8.5/hardware/tools/avr -prefs=runtime.tools.avrdude.path=/home/rokkos/arduino-1.8.5/hardware/tools/avr -verbose /home/rokkos/Documents/C/makerbuino-pong-game/src/pong_game/pong_game.ino
@@ -12,6 +13,6 @@ game_binary=pong_game.ino.hex
 cd /home/rokkos/Documents/C/makerbuino-pong-game/bin
 
 #This command removes all file except one
-shopt -s extglob 
-rm -rf -- !($game_binary) 
-mv pong_game.ino.hex $game_name
+shopt -s extglob #https://www.tecmint.com/delete-all-files-in-directory-except-one-few-file-extensions/ describes how this works
+rm -vrf -- !($game_binary|$game_description) 
+mv $game_binary $game_name
